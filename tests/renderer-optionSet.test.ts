5d7d2601314a77c315f18e20d7d25691fb02b092
asllanmakaj7 <107688754+asllanmakaj7@users.noreply.github.com>
2022-08-24 13:51:27 +0200

    Tests for OptionSets

---
+++ tests/renderer-optionSet.test.ts
@@ -0,0 +1,19 @@
+import { renderOptionSet } from '../src/renderer-optionSet';
+import { OptionSet } from '../src/types';
+/* eslint-disable no-undef */
+describe('renderer-optionSet tests', () => {
+  beforeEach(() => {
+  expect.hasAssertions();
+  });
+
+  describe('renderOptionSet test', () => {
+    test('Should return valid type declaration for choices', async () => {
+        const meta = (await import('./choices.metadata.json')).default as OptionSet[];
+        console.log(meta);
+        const result = renderOptionSet(meta);
+        expect(result).toBeTruthy();
+        expect(result).toContain('const enum ani2001__choicestes');
+        expect(result).toMatchSnapshot();
+    });
+  });
+});
